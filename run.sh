6f0d9a9b67e08a9a21af9fd920e32d32ee2ce556
forks-ecosystem <myshopper.club@gmail.com>
2026-07-27 17:42:18 -0400

    Initial commit: LegacyCoin Miner with Docker and web dashboard

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-./build/legacycoin-miner -wallet=184dD2PVevTjg99r5xKVG9w3CyuE7G62eb.123 -pool=stratum+tcp://forks.life:3032
+#./build/legacycoin-miner -wallet=184dD2PVevTjg99r5xKVG9w3CyuE7G62eb.123 -pool=stratum+tcp://forks.life:3032
+cd ~/legacycoin-miner
+WALLET=Li8Q3GonH6iocGzfZLGxbpjazqnmez8zrJ.20 POOL=stratum+tcp://127.0.0.1:3333 WORKERS=4 docker-compose up -d
+
